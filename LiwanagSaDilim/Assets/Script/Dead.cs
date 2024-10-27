8f537a685e6feecbae47225a922da025d75108ba
AwakenedGunner <bsemc.francojmb@gmail.com>
2024-10-27 17:25:08 +0800

    6.6%
    Life = Fireflies
    Damaged Flicker
    Life = Lights

--- LiwanagSaDilim/Assets/Script/Dead.cs
+++ LiwanagSaDilim/Assets/Script/Dead.cs
@@ -2,36 +2,88 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using TMPro;
 
 
 public class Dead : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     public GameObject dead;
     public GameObject Player;
     public bool isDead;
-    public int lives;
-   
+    public int lives = 5;
+
+    //lives game object
+
+    public GameObject life1;
+    public GameObject life2;
+    public GameObject life3;
+    public GameObject life4;
+    public GameObject life5;
+    public TMP_Text fireflies;
+
+
+    //invulnerable
+  
+    private void Start()
+    {
+        life5.gameObject.SetActive(false);
+        life4.gameObject.SetActive(false);
+        life3.gameObject.SetActive(false);
+        life2.gameObject.SetActive(false);
+        life1.gameObject.SetActive(false);
+
+       
+    }
 
     void Update()
     {
-        if (lives <= 0)
+        fireflies.text = PlayerMovements.lives.ToString("Fireflies: " + PlayerMovements.lives);
+        if (PlayerMovements.lives <= 5)
         {
-           dead.SetActive(true);
-           Player.SetActive(false);
-        }
+            life5.gameObject.SetActive(true);
 
-        
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-           
-            lives -= 1;
 
         }
+        if (PlayerMovements.lives <= 4)
+        {
+            life5.gameObject.SetActive(false);
+            life4.gameObject.SetActive(true);
+            life3.gameObject.SetActive(false);
+
+        }
+        if (PlayerMovements.lives <= 3)
+        {
+            life4.gameObject.SetActive(false);
+            life3.gameObject.SetActive(true);
+            life2.gameObject.SetActive(false);
+
+        }
+        if (PlayerMovements.lives <= 2)
+        {
+            life3.gameObject.SetActive(false);
+            life2.gameObject.SetActive(true);
+            life1.gameObject.SetActive(false);
+
+        }
+        if (PlayerMovements.lives <= 1)
+        {
+
+            life2.gameObject.SetActive(false);
+            life1.gameObject.SetActive(true);
+
+
+        }
+        if (PlayerMovements.lives <= 0)
+        {
+            dead.SetActive(true);
+            Player.SetActive(false);
+        }
+
+
+   
     }
+   
     private void OnTriggerExit2D(Collider2D collision)
     {
         isDead = false;
--- LiwanagSaDilim/Assets/Script/PlayerMovements.cs
+++ LiwanagSaDilim/Assets/Script/PlayerMovements.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 
@@ -7,70 +9,111 @@ public class PlayerMovements : MonoBehaviour
 {
 
     private Rigidbody2D Rigidbody2D;
-    public float speedMovement ;
-    public float jumpForce; 
+    public float speedMovement;
+    public float jumpForce;
     private bool isGrounded = false;
     private bool facingRight = true;
     private float horizontalInput;
-
+    public bool damaged = false;
+    public static int lives = 5;
     // 4 animation
     private Animator animation;
     //private bool grounded;
 
-    void Start (){
+    //invulnerable
+    Renderer rend;
+    Color c;
+        void Start() {
 
         // references for rigidbody and animator from object 
-            Rigidbody2D = GetComponent <Rigidbody2D> ();
-            animation = GetComponent<Animator> ();
-           
-}
-    
-    // Player Movements
-    void Update()
-    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+        animation = GetComponent<Animator>();
+        rend = GetComponent<Renderer>();
+        c = rend.material.color;
+
+        }
+
+
+        void Update()
+        {
         horizontalInput = Input.GetAxis("Horizontal");
-        Rigidbody2D.velocity = new Vector2(Input.GetAxis("Horizontal") * speedMovement , Rigidbody2D.velocity.y);
+        Rigidbody2D.velocity = new Vector2(Input.GetAxis("Horizontal") * speedMovement, Rigidbody2D.velocity.y);
 
         if (Input.GetKey(KeyCode.Space) && isGrounded)
         {
             jump();
-            
+
         }
-
-        Direction();
-
         // set animator parameters
         animation.SetBool("Walk", horizontalInput != 0);
         animation.SetBool("grounded", isGrounded);
+
+        Direction();
+
+        if (damaged == true)
+        {
+
+            StartCoroutine("Invulnerable");
+
+            }
+        
+       
     }
     // player jump
-    private void jump () 
+    private void jump()
     {
 
-        Rigidbody2D.velocity = new Vector2 (Rigidbody2D.velocity.x, jumpForce);
+        Rigidbody2D.velocity = new Vector2(Rigidbody2D.velocity.x, jumpForce);
         // grounded = false;
-        isGrounded = false; 
-        animation.SetTrigger("jumpAni");
+        animation.SetTrigger("jump");
+        isGrounded = false;
+
+
+    }
+    IEnumerator Invulnerable()
+    {
+        Physics2D.IgnoreLayerCollision(7, 8, true);
+        c.a = 0.5f;
+        rend.material.color = c;
+        yield return new WaitForSeconds(3f);
+        Physics2D.IgnoreLayerCollision(7, 8, false);
+        c.a = 1f;
+        rend.material.color = c;
+        damaged = false;
        
     }
 
-    private void OnCollisionEnter2D (Collision2D col) 
+    private void OnCollisionEnter2D(Collision2D col)
     {
-        if(col.gameObject.tag == "Floor")
+        if (col.gameObject.tag == "Floor")
         {
             isGrounded = true;
+        }
+       
+    }
+
+    private void OnTriggerEnter2D (Collider2D col)
+    {
+        if (col.gameObject.tag == "Enemy")
+        {
+            damaged = true;
+            lives -= 1;
         }
     }
 
     private void Direction()
     {
-        if (horizontalInput <0 && facingRight || horizontalInput >0 && !facingRight)
+        if (horizontalInput < 0 && facingRight || horizontalInput > 0 && !facingRight)
         {
             facingRight = !facingRight;
             transform.Rotate(new Vector3(0, 180, 0));
         }
-            
-
     }
 
-}
+   
+
+    }
+   
+   
+
+
