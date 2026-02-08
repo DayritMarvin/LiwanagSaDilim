7b050f72d632d03fd9ddc0499bf4cd1e894256d8
NULLdistry <129368169+NULLdistry@users.noreply.github.com>
2026-02-08 20:52:46 +0800

    Controls Improved
    unfinished

---
+++ LiwanagSaDilim/Assets/1. SCRIPT/PlayerControlsAi.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MovementControlType { None, PC, Mobile, MobileAi, PCAi}
+public class PlayerControlsAi : MonoBehaviour
+{
+    
+}
