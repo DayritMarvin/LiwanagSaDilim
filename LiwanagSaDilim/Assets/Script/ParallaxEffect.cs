fad4e9dc058a1539e0386f47fda06d01367614dd
NULLdistry <129368169+NULLdistry@users.noreply.github.com>
2026-04-10 21:47:18 +0800

    Revert "testCommit"
    This reverts commit 912e705245cd7eada8c3901d7420a02b1bd74214.

--- LiwanagSaDilim/Assets/Script/ParallaxEffect.cs
+++ LiwanagSaDilim/Assets/Script/ParallaxEffect.cs
@@ -10,10 +10,6 @@ public class ParallaxEffect : MonoBehaviour
     void Start()
     {
         startPos = transform.position.x;
-        if(cam == null)
-        {
-            cam = Camera.main.gameObject;
-        }
     }
 
     // Update is called once per frame
