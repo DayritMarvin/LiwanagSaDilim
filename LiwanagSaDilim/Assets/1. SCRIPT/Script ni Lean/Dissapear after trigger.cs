ddada4554a5e4c54ffe5838430bc1e29bd814848
xyosh <bsemc.camenolac@gmail.com>
2026-04-27 08:22:18 +0800

    more update levels

---
+++ LiwanagSaDilim/Assets/1. SCRIPT/Script ni Lean/Dissapear after trigger.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class UnShowGroupOnTrigger : MonoBehaviour
+{
+    private Collider2D[] colliders;
+    private SpriteRenderer[] renderers;
+
+    private void Start()
+    {
+        // 🔒 get all children components
+        colliders = GetComponentsInChildren<Collider2D>();
+        renderers = GetComponentsInChildren<SpriteRenderer>();
+
+        // 🔒 hide everything
+        foreach (Collider2D c in colliders)
+            c.enabled = true;
+
+        foreach (SpriteRenderer r in renderers)
+            r.enabled = true;
+    }
+
+    // 🔥 called by TriggerTrap
+    public void Activate()
+    {
+        foreach (Collider2D c in colliders)
+            c.enabled = false;
+
+        foreach (SpriteRenderer r in renderers)
+            r.enabled = false;
+    }
+}
